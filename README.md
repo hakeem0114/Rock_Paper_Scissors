8ca00ba42f86dc9635524a6075f31d56aae174e9
hakeem <hakeem0114@gmail.com>
2023-01-01 16:22:08 -0800

    Finished Game. First iteration was right but I used a full stop instead of a comma.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Rock_Paper_Scissors
+
+Pseudo Code:
+1. Rules:
+a) Rock beats scissors, scissors beat paper, and paper beats rock.
+
+b) Agree ahead of time whether you’ll count off “rock, paper, scissors, shoot” or just “rock, paper, scissors.”
+
+2. Method:
+a) Prompt user to decide the number of rounds
+b) Player enters input
+c) Computer enter random input
+d) Decision on winner
+e) Assign score & declare winner at the end of the rounds.
--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const choice = ["rock","paper","scissors"];
 
 
-///////////////////Computer///////////////////////////
-function getComputerChoice(choice){
+///////////////////Computer Choice///////////////////////////
+function getComputerChoice(){
     //math.floor = rounds down to the nearest whole number
     //math.random = random number between 0 & <1
     //choice.length = length of the variable
@@ -11,46 +11,82 @@ function getComputerChoice(choice){
    
     let random_val= Math.floor(Math.random()*choice.length);
 
-    let random_choice= choice[random_val]; 
+    let random_choice= choice[random_val]; //Put the value in the choice array to select an object in it.
     // random_choice = choice[0] or [1] or [2] at random
 
     return random_choice;
 }
+////////////////////////////////////////////////////////////////////
 
-console.log(getComputerChoice(choice));
-let comp_choice = Number(getComputerChoice(choice));
-///////////////////////////////////////////////////////////
+///////////////////Player Choice///////////////////////////
+function getPlayerChoice(){
+    let player_choice = prompt("Enter: rock, paper or scissors");
+
+    player_choice = player_choice.toLowerCase(); //Makes user input in lower case
+
+    return player_choice;
+}
+//////////////////////////////////////////////////////////
+
+///////////////////Play Round///////////////////////////
+
+function game_winner(playerSelection, computerSelection){
 
 
-///////////////Player Choice Selection///////////////////
-let player1_choice = prompt("Enter either: Rock, Paper or Scissors");
-console.log(player1_choice);
-let player_choice = Number(player1_choice);
-/////////////////////////////////////////////////////////
+    if(playerSelection == computerSelection){
+        return "Draw";
+    }
+    else if(computerSelection == "rock" && playerSelection == "paper"){
+        return "Player"  ; 
+    }
+    else if(computerSelection == "rock" && playerSelection == "scissors"){
+        return "Computer"  ; 
+    }
+    else if(computerSelection == "scissors" && playerSelection == "paper"){
+        return "Computer"  ; 
+    }
+    else if(computerSelection == "scissors" && playerSelection == "rock"){
+        return "Player" ;  
+    }
+}
 
-//////////////GAME START/////////////////////////////
-let _draw= "DRAW!";
-let win_comp = "COMPUTER WINS";
-let win_play= "PLAYER WINS";
+function playRound(playerSelection, computerSelection){
+    let result = game_winner(playerSelection, computerSelection);
 
-function playRound(comp_choice, player_choice){ let count=0;
-    if(comp_choice == player_choice) 
-        { console.log("DRAW!");count =0; } 
-    if(comp_choice =="ROCK" && player_choice=="SCISSORS")
-        { console.log("COMPUTER WINS!"); count =1;}
-     if(comp_choice =="ROCK" && player_choice =="PAPER")
-        { console.log("PLAYER WINS!"); count =2; }
-    if(comp_choice =="SCISSORS" && player_choice=="ROCK")
-        { console.log("PLAYER WINS!"); count =2; }
-    if(comp_choice =="PAPER" && player_choice =="ROCK")
-        { console.log("COMPUTER WINS!"); count =1;}
-        
-    return count;
-}   
-playRound();
-//if(playRound()==0){console.log(_draw)};
-//if(playRound()==1){console.log(win_comp)};
-//if(playRound()==2){console.log(win_play)};
-///////////////////////////////////////////////////////////
 
-///////////////////LOOP #/////////////////////////
+    if(result == "Draw"){ return "Its a tie!"}
+    else if(result == "Player"){return `You win! ${playerSelection} beats ${computerSelection}`}
+    else if(result == "Computer"){return `You lose! ${computerSelection} beats ${playerSelection}`}
+}
+//////////////////////////////////////////////////////////
+
+///////////////////Game Loop///////////////////////////
+function game(){ //Runs  5 times
+
+    let playerScore=0;
+    let compScore=0;
+    for (let i=0; i<5; i++){
+
+    //Global variables for functions for each iteration
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+    console.log(`Player Selection is ${playerSelection} & Comp Selection is ${computerSelection}`);
+
+        console.log(playRound(playerSelection, computerSelection));
+
+        if(game_winner(playerSelection,computerSelection) == "Player"){
+            playerScore++;
+        }
+
+        if(game_winner(playerSelection, computerSelection) == "Computer"){
+            compScore++;
+        }
+        console.log("End of round" + i);
+    }
+
+    if (playerScore > compScore){return console.log("Player Wins By Knockout!")}
+    else {return console.log("Computer Wins!....Fatality!")}
+}
+
+game();
+////////////////////////////////////////////////////////
